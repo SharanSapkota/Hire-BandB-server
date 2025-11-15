0a0c2b8ba5bd6964bae154934b6c81b75f361609
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-15 11:38:52 +0200

    Add default query in repository

--- src/repositories/bikeRepository.ts
+++ src/repositories/bikeRepository.ts
@@ -9,8 +9,32 @@ export class BikeRepository {
 
   }
 
-  async findBikeById(id: number) {
-    return this.db.bike.findUnique({ where: { id }, include: {bikeImages: true, bikeAddress: true, category: true, owner: { select: { id: true, firstName: true, lastName: true, emails: { where: { isPrimary: true }, select: { email: true } } } } } });
+  async findBikeById(id: number, query: any = null) {
+      if (query) {
+        query = query.build();
+      } else {
+        query = {
+          where: { id },
+          include: {
+            bikeImages: true,
+            bikeAddress: true,
+            category: true,
+            owner: {
+              select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                emails: {
+                  where: { isPrimary: true },
+                  select: { email: true },
+                },
+              },
+            },
+          },
+        };
+      }
+    
+      return await this.db.bike.findUnique(query);
   }
 
   async createBike(data: any, transaction: any) {
@@ -34,3 +58,50 @@ export class BikeRepository {
   }
 }
 
+
+
+// {
+//   id: 21,
+//   name: 'E-scooter',
+//   description: '',
+//   rentAmount: 5,
+//   pricePerHour: 5,
+//   pricePerDay: 5,
+//   autoAccept: false,
+//   status: 'AVAILABLE',
+//   startTime: null,
+//   endTime: null,
+//   categoryId: 1,
+//   ownerId: 14,
+//   createdAt: 2025-11-13T08:52:30.336Z,
+//   updatedAt: 2025-11-13T08:52:30.336Z,
+//   bikeImages: [
+//     {
+//       id: 12,
+//       bikeId: 21,
+//       imageUrl: '/media/Gemini_Generated_Image_e8pudne8pudne8pu-1763023948169-65303092.png'
+//     }
+//   ],
+//   bikeAddress: [
+//     {
+//       id: 16,
+//       bikeId: 21,
+//       latitude: 65.05664240194115,
+//       longitude: 25.45755386352539,
+//       address: 'Kaitoväylä 1, 90590 Oulu, Finland',
+//       street: '1 Kaitoväylä',
+//       city: 'Oulu',
+//       state: 'Pohjois-Pohjanmaa',
+//       country: 'Finland',
+//       postalCode: '90590',
+//       placeId: null
+//     }
+//   ],
+//   category: { id: 1, name: 'City' },
+//   owner: {
+//     id: 14,
+//     firstName: 'Ram',
+//     lastName: 'Sapkota',
+//     emails: [ [Object] ]
+//   }
+// }
