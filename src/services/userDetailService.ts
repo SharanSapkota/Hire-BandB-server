0df2351c0e27625b871faec85199505d74145f59
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-16 19:15:24 +0200

    Remove user Details

--- src/services/userDetailService.ts
+++
@@ -1,9 +0,0 @@
-import * as repo from '../repositories/userDetailRepository';
-
-export function getDetail(userId: number) {
-  return repo.findUserDetailByUserId(userId);
-}
-
-export function upsertDetail(userId: number, data: any) {
-  return repo.upsertUserDetail(userId, data);
-}
