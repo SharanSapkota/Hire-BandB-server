b8965f7e7bec84083b328462d5ebee9f15a2ec41
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-17 22:52:21 +0200

    Remove unwanted code

--- src/routes/userDetails.ts
+++ src/routes/userDetails.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
-import * as ctrl from '../controllers/userDetailController';
+// import { Router } from 'express';
+// import * as ctrl from '../controllers/userDetailController';
 
-const router = Router();
+// const router = Router();
 
-router.get('/:userId', ctrl.get);
-router.post('/:userId', ctrl.upsert);
-router.put('/:userId', ctrl.upsert);
+// router.get('/:userId', ctrl.get);
+// router.post('/:userId', ctrl.upsert);
+// router.put('/:userId', ctrl.upsert);
 
-export default router;
+// export default router;
