cc48e043b5654d7dc18a56edb7dac80584c8f8bd
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-17 22:41:42 +0200

    Add user servce

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,4 +3,16 @@ export class UserService {
   async getUserById(userId: number) {
     return userRepo.findUserById(userId);
   }
+
+  async createUserPaymentMode(data: { type: string, name: string, isVerified: boolean, userId: number, accountNumber?: string, verificationMethod?: string, customerId?: string }) {
+    return userRepo.createUserPaymentMode(data);
+  }
+
+  async getUserPaymentModeByCustomerId(customerId: string) {
+    return userRepo.getUserPaymentModeByCustomerId(customerId);
+  }
+
+  async updateUserPaymentMode(id: string, data: { isVerified: boolean }) {
+    return userRepo.updateUserPaymentMode(id, data);
+  }
 }
